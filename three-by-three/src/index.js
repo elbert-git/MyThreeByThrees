8a79ec8ce75713e4571a0d82352d4378fa9f243a
elbert-git <elbertwithane@gmail.com>
2021-10-25 10:05:38 +0800

    initialised the project

---
+++ three-by-three/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+//create header
+//create bottom div
+//create board
+//create tile
+
+
+
+// ----------------- react dom ----------
+class Main extends React.Component{
+	render(){
+		return(
+			<div>
+				hello
+			</div>
+		)
+	}
+}
+ReactDOM.render(
+  <Main />,
+  document.getElementById('root')
+);
